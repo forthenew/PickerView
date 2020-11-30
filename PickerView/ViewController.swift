acc9c39ee41773c43aef6c7d755616cd6f6dea7d
forthenew <forthenew@gmail.com>
2020-11-30 23:58:28 +0900

    피커 뷰 룰렛에 파일명 대신 이미지 출력하기

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     let MAX_ARRAY_NUM = 11
     let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HIGHT:CGFloat = 80
     var imageArray = [UIImage?]()
     var imageFileNames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"]
     
@@ -37,13 +38,24 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return imageFileNames.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return imageFileNames[row]
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HIGHT
     }
+    
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return imageFileNames[row]
+//    }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         lblImageFileName.text = imageFileNames[row]
         imageView.image = imageArray[row]
+    }
+    
+
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 150)
+        return imageView
     }
 }
 
